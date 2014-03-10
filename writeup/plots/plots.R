a2cce890f04bcf8a943d2e8500d9bebbfd080aeb
rdaland <r.daland@gmail.com>
2014-03-10 15:21:54 -0700

    Added ggplot call to plot.R, also dump the Zipf frequency dist in plotting. Fixed minor digit typo in scores.csv.

--- writeup/plots/plots.R
+++ writeup/plots/plots.R
@@ -1,10 +1,19 @@
 library(ggplot2)
-# will need to adjust this to your directory structure
-setwd("~/research/ArtificialLanguages/writeup/plots/")
-data <- read.csv("scores.csv",sep="\t",header=T)
 
+# setwd("~/Desktop/artlangseg/writeup/plots/")
+dat <- read.csv("scores.csv",sep="\t",header=T)
 
-ggplot(data=data,aes(x=struct,y=tf,colour=lang))+geom_line()+facet_grid(facets=dist~.)
+# ggplot(data=data,aes(x=struct,y=tf,colour=lang))+geom_line()+facet_grid(facets=dist~.)
+# simple = lm(tf~nse,data=data)
+# cor.test(data$tf,data$nse)
 
-simple = lm(tf~nse,data=data)
-cor.test(data$tf,data$nse)
+dat = dat[dat$dist != "zipf",]
+dat$dist = factor(dat$dist)
+
+pdf("roberts_second_try.pdf")
+ggplot(dat) +
+  geom_line(aes(x=tl, y=tf, linetype=lang)) +
+  geom_point(aes(x=tl, y=nse, shape=lang)) +
+  facet_grid(dist ~ .)
+dev.off()
+
