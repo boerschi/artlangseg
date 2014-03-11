68e9038eec2eff134e371e5134f36678668c2180
Benjamin Börschinger <benjamin.boerschinger@googlemail.com>
2014-03-11 10:19:45 +0100

    included plot in paper, cut Zipf-discussion, fit everything into 5 pages.

--- writeup/plots/plots.R
+++ writeup/plots/plots.R
@@ -1,6 +1,7 @@
 library(ggplot2)
 
 # setwd("~/Desktop/artlangseg/writeup/plots/")
+setwd("~/research/ArtificialLanguages/writeup/plots/")
 dat <- read.csv("scores.csv",sep="\t",header=T)
 
 # ggplot(data=data,aes(x=struct,y=tf,colour=lang))+geom_line()+facet_grid(facets=dist~.)
@@ -10,10 +11,15 @@ dat <- read.csv("scores.csv",sep="\t",header=T)
 dat = dat[dat$dist != "zipf",]
 dat$dist = factor(dat$dist)
 
-pdf("roberts_second_try.pdf")
+pdf("bens_version.pdf")
 ggplot(dat) +
-  geom_line(aes(x=tl, y=tf, linetype=lang)) +
-  geom_point(aes(x=tl, y=nse, shape=lang)) +
-  facet_grid(dist ~ .)
-dev.off()
+  geom_line(aes(x=token.length, y=token.f.score, linetype=language, colour=language),size=1.5) +
+  geom_point(aes(x=token.length, y=token.f.score, colour=language),size=3.0,alpha=I(0.5)) +
+  geom_point(aes(x=token.length, y=nse, shape=language, colour=language),size=3.5,alpha=I(0.8)) +
+  facet_grid(dist ~ .) +
+  xlab("token length") +
+  ylab("token f-score and NSE") +
+  opts(legend.position="top") +
+  scale_y_continuous(breaks=seq(0,1,0.1))
+  dev.off()
 
